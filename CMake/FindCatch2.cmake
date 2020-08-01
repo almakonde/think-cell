23ee249f92725ff1394d25736bbc231727329571
david.mccabe <david.mccabe082@gmail.com>
2020-08-01 14:21:05 +0100

    [PREP] added test framework

---
+++ CMake/FindCatch2.cmake
@@ -0,0 +1,27 @@
+find_path(Catch2_INCLUDE_DIR
+NAMES
+  catch.hpp
+PATHS
+  ${CMAKE_SOURCE_DIR}/external/Catch2/include
+)
+
+set(Catch2_VERSION "2.13")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Catch2
+FOUND_VAR Catch2_FOUND
+REQUIRED_VARS
+  Catch2_INCLUDE_DIR
+)
+
+if (Catch2_FOUND AND NOT TARGET ::Catch2)
+  add_library(::Catch2 INTERFACE IMPORTED)
+  set_target_properties(::Catch2 PROPERTIES
+  INTERFACE_INCLUDE_DIRECTORIES
+    ${Catch2_INCLUDE_DIR}
+  )
+endif()
+
+mark_as_advanced(
+    Catch2_INCLUDE_DIR
+)
