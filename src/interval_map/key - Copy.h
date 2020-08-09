864d0d3d37612b84befd5c1fea1a5f73fab0dbc2
david.mccabe <david.mccabe082@gmail.com>
2020-08-09 14:16:13 +0100

    [TEST] added value class for checking value constraints

---
+++ src/interval_map/key - Copy.h
@@ -0,0 +1,22 @@
+#pragma once
+
+template<typename V>
+class Value
+{
+public:
+	Value(V val) : _val(val) {}
+    
+    // copyable
+	Value(V& k) : _val(v._val) {}
+    
+    // assignable
+	void operator=(const Value<V>& v) { _val = v._val; }
+
+    // comparable via operator==
+    bool operator==(const Value<V>& v) const { return _val == v._val; }
+
+private:
+    V _val;
+	Value() = delete;
+};
+
---
+++ src/interval_map_test/value.test.cpp
@@ -0,0 +1,52 @@
+
+#include "catch.hpp"
+#include "interval_map/Value.h"
+
+TEST_CASE("check value constraints", "[Value]") {
+
+	Value<int> v1(1);
+	Value<int> v2(2);
+
+	SECTION("Value is comparible via operator==") {
+
+		try
+		{
+			Value<int> one1(1);
+			Value<int> one2(1);
+			REQUIRE(one1==one2);
+		}
+		catch (...)
+		{
+			REQUIRE(false);
+		}
+	}
+
+	SECTION("keys are assignable") {
+
+		try
+		{
+			v1 = v2;
+
+			REQUIRE(v1==v2);
+		}
+		catch (...)
+		{
+			REQUIRE(false);
+		}
+	}
+
+	SECTION("keys are copyable") {
+
+		try
+		{
+			Value v3(v1);
+
+			REQUIRE(v1==v3);
+		}
+		catch (...)
+		{
+			REQUIRE(false);
+		}
+	}
+
+}
